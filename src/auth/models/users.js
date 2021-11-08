f862180b1024449bd076c8951832ae7bf475f939
Wesam-Alqawasmeh <alqawasmehwesam@gmail.com>
2021-11-08 20:53:26 +0200

    lab 07 - bearer

---
+++ src/auth/models/users.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const API_SECRET = process.env.SECRET
+
+const userSchema = (sequelize, DataTypes) => {
+  const model = sequelize.define('User', {
+    username: { type: DataTypes.STRING, allowNull: false, unique: true },
+    password: { type: DataTypes.STRING, allowNull: false, },
+    token: {
+      type: DataTypes.VIRTUAL,
+      get() {
+        return jwt.sign({ username: this.username }, API_SECRET);
+      }
+    }
+  });
+
+  // model.beforeCreate(async (user) => {
+  //   let hashedPass = bcrypt.hash(user.password, 5);
+  //   user.password = hashedPass;
+  // });
+
+  // Basic AUTH: Validating strings (username, password) 
+  model.authenticateBasic = async function (username, password) {
+    const user = await this.findOne({where:{ username: username }});
+    console.log(user);
+    console.log(password);
+    console.log(user.password);
+    const valid = await bcrypt.compare(password, user.password)
+    if (valid) { return user; }
+    throw new Error('Invalid User');
+    // if (valid) {
+    //   let newToken = jwt.sign({ username: user.username }, API_SECRET);
+    //   user.token = newToken;
+    //   return user;
+    // } else {
+    //   throw new Error('Invalid User');
+    // }
+  }
+
+  // Bearer AUTH: Validating a token
+  model.authenticateBearer = async function (token) {
+    try {
+      const parsedToken = jwt.verify(token, API_SECRET);
+      const user = await this.findOne({where: { username: parsedToken.username }})
+      if (user) { return user; }
+      throw new Error("User Not Found");
+    } catch (e) {
+      throw new Error(e.message)
+    }
+  }
+
+  return model;
+}
+
+module.exports = userSchema;
---
+++ src/auth/routes.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const express = require('express');
+const authRouter = express.Router();
+
+const { users } = require('./models/index.js');
+const basicAuth = require('./middleware/basic.js');
+const bearerAuth = require('./middleware/bearer.js');
+const bcrypt = require('bcrypt');
+
+authRouter.post('/signup', async (req, res, next) => {
+  try {
+    req.body.password = await bcrypt.hash(req.body.password, 5);
+    let userRecord = await users.create(req.body);
+    const output = {
+      user: userRecord,
+      token: userRecord.token
+    };
+    res.status(201).json(output);
+  } catch (e) {
+    next(e.message);
+  }
+});
+
+authRouter.post('/signin', basicAuth, (req, res, next) => {
+  const user = {
+    user: req.user,
+    token: req.user.token
+  };
+  res.status(200).json(user);
+});
+
+authRouter.get('/users', bearerAuth, async (req, res, next) => {
+  // const users = await Users.findAll({});
+  // const list = users.map(user => user.username);
+  // res.status(200).json(list);
+  res.status(200).json({
+    'message': 'You are authorized to view the user profile',
+    'user': req.user
+  })
+});
+
+authRouter.get('/secret', bearerAuth, async (req, res, next) => {
+  res.status(200).send("Welcome to the secret area!")
+});
+
+
+module.exports = authRouter;
